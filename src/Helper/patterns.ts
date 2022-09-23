af2bbe2cfc37a2d73974cca965da6b0e83ad0367
Zach Hutz <zachhutz0@gmail.com>
2022-09-23 18:31:39 -0400

    fixed some patterns

--- src/Helper/patterns.ts
+++ src/Helper/patterns.ts
@@ -1,5 +1,5 @@
 export const redditMoment = /reddit.com/;
 export const bts = /bts/;
-export const jesus = /jesus/;
-export const xi = /xi|jinping|winnie the pooh/;
-export const liberal = /liberal/;
+export const jesus = /(?<!\S)jesus(?!\S)/;
+export const xi = /(?<!\S)xi(?!\S)|jinping|winnie the pooh/;
+export const liberal = /(?<!\S)liberal(?!\S)/;
