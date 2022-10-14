93bcffaf57cd2431b13eb3894bf2f42bd357a797
Zach Hutz <zachhutz0@gmail.com>
2022-10-14 16:21:36 -0400

    pattern for future use

--- src/Helper/patterns.ts
+++ src/Helper/patterns.ts
@@ -3,3 +3,4 @@ export const bts = /bts/;
 export const jesus = /(?<!\S)jesus(?!\S)/;
 export const xi = /(?<!\S)xi(?!\S)|jinping|winnie the pooh/;
 export const liberal = /(?<!\S)liberal(?!\S)/;
+//export const math = /(\d+\s*[+\-*/^]\s*)+\d+$/;
