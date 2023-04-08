cdff5430bda1ea61cc15320166cb81ba10889133
Zach <zachhutz0@gmail.com>
2023-04-08 10:25:50 -0400

    updated help

--- src/Commands/help.ts
+++ src/Commands/help.ts
@@ -13,8 +13,6 @@ module.exports = {
 				`
 $help: Brings up this help page
 $ping: Pong obviously
-$crypto: Market data on select cryptocurrencies
-$persistentcrypto: Same as $crypto but refreshes every 15 seconds
 $mp4 <url>: Takes a YouTube link and replies with the downloaded video
 $mp3 <url>: Takes a YouTube link and replies with an mp3
 $rammap <speed min> <speed max> <CAS min> <CAS max>: Creates a table comparing true RAM latency, in nanoseconds, in the specified range of values in megatransfers (MT) and CAS latency.
